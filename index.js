d91ec8ab4c1cd6c92599bdc10ba6379c211aa9f3
Bippserino <bobomandic2808@gmail.com>
2020-12-11 22:16:25 +0100

    Added screen

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-
 function setClock() {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    const hours = ("0" + currentDate.getHours()).slice(-2)
+    const minutes = ("0" + currentDate.getMinutes()).slice(-2)
+    const day = ("0" + currentDate.getDate()).slice(-2)
+    const month = ("0" + (currentDate.getMonth() + 1)).slice(-2)
+    const year = currentDate.getFullYear()
+    
         
     setRotation(".clock-second-hand", secondsRatio)
     setRotation(".clock-minute-hand", minutesRatio)
     setRotation(".clock-hour-hand", hoursRatio)
+    $(".toolbar-clock").text(hours + ":" + minutes)
+    $(".date").text(day + "." + month + "." + year + ".")
 }
 
 function setRotation(element, rotationRatio) {
     $(element).get(0).style.setProperty("--rotate", rotationRatio * 360);
 }
-
 
 function swingAnimation() {
     $(".picture-on-wall").hover(function() {
@@ -25,10 +32,13 @@ function swingAnimation() {
     });
 }
 
-
-$(document).ready(function() {
+function clock() {
     setClock()
     setInterval(setClock, 1000)
+}
+
+$(document).ready(function() {
+    clock()
     swingAnimation()
 });
    
