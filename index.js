0065e683f721e64e21e1073218dbe041c3aff7ba
Bippserino <bobomandic2808@gmail.com>
2020-12-12 17:19:33 +0100

    Added functionality to desktop icons

--- index.js
+++ index.js
@@ -37,8 +37,100 @@ function clock() {
     setInterval(setClock, 1000)
 }
 
+function toolbarIconEL() {
+    $(".icon-box").click((event) => {
+        $(".active-icon").removeClass("active-icon")
+        $(event.currentTarget).addClass("active-icon")
+        $(".screen-window").remove()
+        $(".icon-box:last-child").remove()
+    })
+}
+
+function desktopIconEL() {
+    $(".desktop-icon-box").click((event) => {
+        let iconPath = event.currentTarget.classList[2]
+        $(".active-icon").removeClass("active-icon")
+
+        if (iconPath === "folder") {
+            $(".icons").append(`<div class="icon-box">
+                <img src="img/${iconPath}-icon.svg" alt="${iconPath} icon" class="icon ${iconPath}" draggable="false"></div>`)
+            $(".icon-box:last-child").addClass("active-icon")
+        }
+
+        if (iconPath === "pdf") {
+            window.open("https://drive.google.com/file/d/1Lksm5Z0q2RS_O4w9n68WfkYlq1NSeVUS/view?usp=sharing","_blank")
+        }
+    })
+}
+
+function folderEL() {
+    $(".folder-icon-box ").click((event) => {
+        switch (event.currentTarget.classList[1]) {
+            case "html-certificate":
+                window.open("https://drive.google.com/file/d/1jBywQLzUrpU7QRoFSB9PpIt5c34VNNGb/view?usp=sharing","_blank")
+                break;
+            
+            case "python-certificate":
+                window.open("https://drive.google.com/file/d/1Ww_esPAXZHUHQPQD9ace_ykMR0R_bvor/view?usp=sharing","_blank")
+                break;
+
+            case "ieeextreme-certificate":
+                window.open("https://drive.google.com/file/d/13X4rb8wLNGjp8FGrgjmpB7958nZ-mo1b/view?usp=sharing","_blank")
+                break;
+        }
+    })
+}
+
+function iconEL() {
+    toolbarIconEL()
+    desktopIconEL()
+}
+
+function addFolderViewEL() {
+    $(".folder").click((event) => {
+        let folderName = event.currentTarget.classList[1]
+        folderName = folderName.charAt(0).toUpperCase() + folderName.slice(1)
+
+        $(".desktop").append(`<div class="screen-window folder"><div class="window-head">
+            <p class="window-title">${folderName}</p>
+            <img src="img/exit.svg" alt="exit" class="exit-icon">
+        </div>
+        <div class="window-path">
+            <p>C:\\Users\\Bojan\\Desktop\\${folderName}\\</p> 
+        </div>
+        <div class="folder-icon-box html-certificate">
+            <img src="img/pdf.svg" alt="certificates icon" class="desktop-icon-img certificate" draggable="false">
+            <p class="title">Coursera-HTML 5</p>
+        </div>
+        <div class="folder-icon-box python-certificate">
+            <img src="img/pdf.svg" alt="certificates icon" class="desktop-icon-img certificate" draggable="false">
+            <p class="title">Coursera-Python</p>
+        </div>
+        <div class="folder-icon-box ieeextreme-certificate">
+            <img src="img/pdf.svg" alt="certificates icon" class="desktop-icon-img certificate" draggable="false">
+            <p class="title">IEEExtreme 13.0</p>
+        </div>
+    </div>`)
+    addExitFolderEL()
+    folderEL()
+    })
+}
+
+// Handle press on exit button
+function addExitFolderEL() {
+    $(".exit-icon").click(() => {
+        $(".screen-window").remove()
+        $(".active-icon").removeClass("active-icon")
+        $(".icon-box:last-child").remove()
+        $(".windows").addClass("active-icon")
+        
+    })
+}
+
 $(document).ready(function() {
     clock()
     swingAnimation()
+    iconEL()
+    addFolderViewEL()
 });
    
